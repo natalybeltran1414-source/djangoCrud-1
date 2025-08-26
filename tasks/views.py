8adc2d9fb5872c72e6c77eab003512e855ba4f89
davidnavarrete <davidnava1998efrain@outlook.com>
2025-08-26 10:28:13 -0500

    version 1
    vistas html

---
+++ tasks/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+def signup(request):
+    return render(request, 
+                  "signup.html",
+                  {"form":UserCreationForm})
+
+
